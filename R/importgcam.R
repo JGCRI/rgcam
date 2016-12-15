2946976ef294ea52669d122c724996cd44d65897
Robert Link <robert.link@pnnl.gov>
2016-12-15 08:01:04 -0500

    Refactor GCAM data management into its own package.
    This project started out as a visualization application, but as the
    development progressed, it became clear that the data management
    tools would be useful in their own right and therefore needed to go
    into a separate package.

--- R/importgcam.R
+++ R/importgcam.R
@@ -1,47 +1,6 @@
-#' Load a project data file.
-#'
-#' This function will load a project data file and return the project data
-#' structure.  Project files can be created with \code{\link{addScenario}}.
-#'
-#' Project data is stored as an R 'save' file with a single variable called
-#' 'prjdata'.  If the data file doesn't have the expected variable in it, the
-#' function will throw an error.  It also does some minimal checks to ensure
-#' that the data is valid, but there is a limit to what it can do.
-#' Specifically, it checks to see that the structure loaded is:
-#'     1. a list,
-#'     2. with at least one element, and
-#'     3.   all of those elements are lists,
-#'     4.   with at least one element, and
-#'     5.     all of those elements are data frames,
-#'     6.     with a scenario column, and
-#'     7.     at least one other column.
-#' If the validation fails, the error message will indicate which of these steps
-#' it failed on.
-#'
-#' Despite these checks, it is possible to construct a data set that passes and
-#' yet still contains bad data.  When in doubt load the file directly and check
-#' to see that it contains the data you expect it to.
-#' @param projFile The file to load the data from.
-#' @export
-loadProject <- function(projFile) {
-    load(projFile)                  # Loads the variable prjdata.
-    if(!exists("prjdata", inherits=FALSE)) {
-        ## Something went wrong with the load.  Probably projFile exists but
-        ## isn't a valid project file.
-        message(paste("File", projFile,
-                      "does not contain valid project data."))
-        message("Try loading the file into an R session and verify that it contains the variable 'prjdata'.")
-        stop(paste("Unable to load project file", projFile))
-    }
-
-    stat <- project.valid(prjdata)
-    if(stat != 0) {
-        stop(paste("Invalid project data in",
-                   projFile,".  Validation failed at step", stat))
-    }
-
-    prjdata
-}
+################################################################################
+#### importgcam.R:  Functions related to importing data from GCAM databases.
+################################################################################
 
 #' Add a scenario from a GCAM output database to a project data set
 #'
@@ -65,6 +24,16 @@ loadProject <- function(projFile) {
 #' will fail unless \code{clobber = TRUE}, in which case the old scenario will
 #' be silently overwritten.
 #'
+#' You may optionally specify transformations to apply to the tables returned by
+#' the model interface.  Examples of transformations you might want to apply
+#' include aggregating values or dropping unused columns.  Specify
+#' transformations as a named list of function objects, where the names in the
+#' list indicate which queries the transformations will be applied to.  Each
+#' function should take a single argument, which will be the original table and
+#' should return the modified table as a data frame.  Do not drop the "scenario"
+#' column as part of one of your transformations; certain types of plots need
+#' it.
+#'
 #' If everything goes as expected, the new scenario will be added to the data
 #' set and written back into the project data file.  The new data set will also
 #' be returned from the function so that it can be used without having to reread
@@ -81,11 +50,13 @@ loadProject <- function(projFile) {
 #' name; otherwise, fail if \code{scenario} already exists in the data set.
 #' @param miclasspath Java class path for the GCAM Model Interface.  If
 #' \code{NULL}, use a bult-in default.
+#' @param transformations Transformation functions to apply to the queries (see
+#' details).
 #' @return The project dataset with the new scenario added.
 #' @importFrom dplyr %>%
 #' @export
 addScenario <- function(dbFile, projFile, scenario=NULL, queryFile=NULL,
-                        clobber=FALSE, mijar=NULL) {
+                        clobber=FALSE, mijar=NULL, transformations=NULL) {
     if(file.exists(projFile)) {
         if(file.access(projFile, mode=6)!=0) { # 6 == read and write permission
             ## file.access returns 0 on success
@@ -138,6 +109,20 @@ addScenario <- function(dbFile, projFile, scenario=NULL, queryFile=NULL,
 
     tables <- lapply(tables, table.scen.trim)
 
+    ## apply transformations, if any
+    if(!is.null(transformations)) {
+        for(xform in names(transformations)) {
+            if(! xform %in% names(tables)) {
+                warning("addScenario: cannot apply transform", xform,
+                        ". No such table in data set.")
+            }
+            else {
+                ## apply this transformation to the query of the same name and
+                ## store it back in its original slot.
+                tables[[xform]] <- transformations[[xform]](tables[[xform]])
+            }
+        }
+    }
 
     attr(tables, 'date') <- scen$date
     prjdata[[scen$scenario]] <- tables
@@ -145,51 +130,6 @@ addScenario <- function(dbFile, projFile, scenario=NULL, queryFile=NULL,
     save(prjdata, file=projFile, compress='xz')
 
     prjdata
-}
-
-
-#' List the scenarios in a project data set
-#'
-#' Return the names of the scenarios available in a project data set.  The input
-#' can be either the name of a file containing the data set, or the data
-#' structure previously loaded from such a file.
-#'
-#' @param projData The data set to report on.
-#' @export
-listScenarios <- function(projData) {
-    if(is.character(projData)) {
-        pd <- load(projData)
-    }
-    else {
-        pd <- projData
-    }
-
-    names(pd)
-}
-
-
-#' List the queries available for a scenario
-#'
-#' Return the names of the queries available for a scenario in a project data
-#' set.  Unlike \code{\link{listScenarios}}, this function requires the data set
-#' to have been previously loaded, so it cannot take a file name.
-#'
-#' @param projData The data set to report on.
-#' @param scenario The name of the scenario to report on.
-#' @export
-listQueries <- function(projData, scenario) {
-    if(is.character(projData)) {
-        stop('This function requires the data set to have been already loaded.')
-    }
-
-    if(scenario %in% names(projData)) {
-        names(projData[[scenario]])
-    }
-    else {
-        msg <- paste("Scenario", scenario, "is not in this data set.")
-        warn(msg)
-        NULL
-    }
 }
 
 
@@ -274,65 +214,6 @@ table.scen.trim <- function(tbl) {
     dplyr::mutate(tbl, scenario=sep.date(scenario)[['scenario']])
 }
 
-#' Check whether a project data structure is valid.
-#'
-#' This function will check the 7 conditions described in
-#' \code{\link{loadProject}}.  It will return 0 if they pass, or the number of
-#' the first condition to fail otherwise.
-#' @param prjdata The data structure to test.
-project.valid <- function(prjdata) {
-    if(!is.list(prjdata))
-        return(1)
-
-    if(length(prjdata) == 0)
-        return(2)
-
-    scenario.checks <- sapply(prjdata, scenario.valid)
-    if(any(scenario.checks>0))
-        return(min(scenario.checks[scenario.checks>0]))
-    else
-        return(0)
-}
-
-#' Check whether a scenario component in a project data structure is valid.
-#'
-#' This function will check conditions 3-7 for a single scenario component in a
-#' project data structure.  It will return the number of the first check to
-#' fail, or 0 if all pass.
-#'
-#' @param scendata The data structure to test
-scenario.valid <- function(scendata) {
-    if(!is.list(scendata))
-        return(3)
-
-    if(length(scendata) == 0)
-        return(4)
-
-    query.checks <- sapply(scendata, query.valid)
-    if(any(query.checks>0))
-        return (min(query.checks[query.checks>0]))
-    else
-        return(0)
-}
-
-#' Check whether a query table in a scenario component structure is valid.
-#'
-#' This function will check conditions 5-7 for a single query table in a
-#' scenario data structure.  It will return the number of the first check to
-#' fail, or 0 if all pass.
-#' @param querytable The query table to test.
-query.valid <- function(querytable) {
-    if(!is.data.frame(querytable))
-        return(5)
-
-    if(! 'scenario' %in% names(querytable))
-        return(6)
-
-    if(ncol(querytable) < 2)
-        return(7)
-    else
-        return(0)
-}
 
 #' Default java class path for running the Model Interface
 #'
@@ -342,10 +223,10 @@ query.valid <- function(querytable) {
 #' customized version of the model interface for some reason, this should take
 #' care of all your GCAM query needs.
 DEFAULT.MICLASSPATH <- paste0(system.file("ModelInterface", "jars",
-                                          package="GCAMdashboard"),"/\\*:",
+                                          package="rgcam"),"/\\*:",
                               system.file("ModelInterface",
                                           "ModelInterface.jar",
-                                          package="GCAMdashboard"))
+                                          package="rgcam"))
 
 #' Sample GCAM database file.
 #'
@@ -354,14 +235,14 @@ DEFAULT.MICLASSPATH <- paste0(system.file("ModelInterface", "jars",
 #' database has been stripped out to control the total size, so a lot of queries
 #' won't work properly.  The \code{\link{SAMPLE.QUERIES}} should all work.
 SAMPLE.GCAMDB <- system.file("extdata","sample_basexdb",
-                             package="GCAMdashboard")
+                             package="rgcam")
 
 #' Sample GCAM query file
 #'
 #' These queries should work with the sample data in
 #' \code{\link{SAMPLE.GCAMDB}}.
 SAMPLE.QUERIES <- system.file("ModelInterface", "sample-queries.xml",
-                              package="GCAMdashboard")
+                              package="rgcam")
 
 #' Prototype Model Interface batch file
 #'
@@ -371,4 +252,4 @@ SAMPLE.QUERIES <- system.file("ModelInterface", "sample-queries.xml",
 #' queries we are trying to run.
 PROTOTYPE.MIBATCH <- readLines(system.file("ModelInterface",
                                            "batch-prototype.xml",
-                                           package="GCAMdashboard"))
+                                           package="rgcam"))
---
+++ R/manageProjectData.R
@@ -0,0 +1,153 @@
+################################################################################
+#### manageProjectData.R:  Functions for managing project data
+################################################################################
+
+#' Load a project data file.
+#'
+#' This function will load a project data file and return the project data
+#' structure.  Project files can be created with \code{\link{addScenario}}.
+#'
+#' Project data is stored as an R 'save' file with a single variable called
+#' 'prjdata'.  If the data file doesn't have the expected variable in it, the
+#' function will throw an error.  It also does some minimal checks to ensure
+#' that the data is valid, but there is a limit to what it can do.
+#' Specifically, it checks to see that the structure loaded is:
+#'     1. a list,
+#'     2. with at least one element, and
+#'     3.   all of those elements are lists,
+#'     4.   with at least one element, and
+#'     5.     all of those elements are data frames,
+#'     6.     with a scenario column, and
+#'     7.     at least one other column.
+#' If the validation fails, the error message will indicate which of these steps
+#' it failed on.
+#'
+#' Despite these checks, it is possible to construct a data set that passes and
+#' yet still contains bad data.  When in doubt load the file directly and check
+#' to see that it contains the data you expect it to.
+#' @param projFile The file to load the data from.
+#' @export
+loadProject <- function(projFile) {
+    load(projFile)                  # Loads the variable prjdata.
+    if(!exists("prjdata", inherits=FALSE)) {
+        ## Something went wrong with the load.  Probably projFile exists but
+        ## isn't a valid project file.
+        message(paste("File", projFile,
+                      "does not contain valid project data."))
+        message("Try loading the file into an R session and verify that it contains the variable 'prjdata'.")
+        stop(paste("Unable to load project file", projFile))
+    }
+
+    stat <- project.valid(prjdata)
+    if(stat != 0) {
+        stop(paste("Invalid project data in",
+                   projFile,".  Validation failed at step", stat))
+    }
+
+    prjdata
+}
+
+
+#' List the scenarios in a project data set
+#'
+#' Return the names of the scenarios available in a project data set.  The input
+#' can be either the name of a file containing the data set, or the data
+#' structure previously loaded from such a file.
+#'
+#' @param projData The data set to report on.
+#' @export
+listScenarios <- function(projData) {
+    if(is.character(projData)) {
+        pd <- load(projData)
+    }
+    else {
+        pd <- projData
+    }
+
+    names(pd)
+}
+
+
+#' List the queries available for a scenario
+#'
+#' Return the names of the queries available for a scenario in a project data
+#' set.  Unlike \code{\link{listScenarios}}, this function requires the data set
+#' to have been previously loaded, so it cannot take a file name.
+#'
+#' @param projData The data set to report on.
+#' @param scenario The name of the scenario to report on.
+#' @export
+listQueries <- function(projData, scenario) {
+    if(is.character(projData)) {
+        stop('This function requires the data set to have been already loaded.')
+    }
+
+    if(scenario %in% names(projData)) {
+        names(projData[[scenario]])
+    }
+    else {
+        msg <- paste("Scenario", scenario, "is not in this data set.")
+        warn(msg)
+        NULL
+    }
+}
+
+#' Check whether a project data structure is valid.
+#'
+#' This function will check the 7 conditions described in
+#' \code{\link{loadProject}}.  It will return 0 if they pass, or the number of
+#' the first condition to fail otherwise.
+#' @param prjdata The data structure to test.
+project.valid <- function(prjdata) {
+    if(!is.list(prjdata))
+        return(1)
+
+    if(length(prjdata) == 0)
+        return(2)
+
+    scenario.checks <- sapply(prjdata, scenario.valid)
+    if(any(scenario.checks>0))
+        return(min(scenario.checks[scenario.checks>0]))
+    else
+        return(0)
+}
+
+#' Check whether a scenario component in a project data structure is valid.
+#'
+#' This function will check conditions 3-7 for a single scenario component in a
+#' project data structure.  It will return the number of the first check to
+#' fail, or 0 if all pass.
+#'
+#' @param scendata The data structure to test
+scenario.valid <- function(scendata) {
+    if(!is.list(scendata))
+        return(3)
+
+    if(length(scendata) == 0)
+        return(4)
+
+    query.checks <- sapply(scendata, query.valid)
+    if(any(query.checks>0))
+        return (min(query.checks[query.checks>0]))
+    else
+        return(0)
+}
+
+#' Check whether a query table in a scenario component structure is valid.
+#'
+#' This function will check conditions 5-7 for a single query table in a
+#' scenario data structure.  It will return the number of the first check to
+#' fail, or 0 if all pass.
+#' @param querytable The query table to test.
+query.valid <- function(querytable) {
+    if(!is.data.frame(querytable))
+        return(5)
+
+    if(! 'scenario' %in% names(querytable))
+        return(6)
+
+    if(ncol(querytable) < 2)
+        return(7)
+    else
+        return(0)
+}
